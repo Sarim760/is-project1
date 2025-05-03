b97732d1cd41cfcc1dcc4686dae4b43a99c1c304
sami0100 <samiabbasi3983@gmail.com>
2025-05-03 19:42:28 +0500

    Initial app setup and navigation structure

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class StegoApp extends StatelessWidget {
     return MaterialApp(
       title: 'Text Steganography',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home:HomePage(),
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
